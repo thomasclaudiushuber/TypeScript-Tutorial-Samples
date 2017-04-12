bcb3dfa06e8041d389c5be993e8b1238432e4935
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-04-12 18:27:02 +0200

    initial commit

---
+++ 08_Module/01_modules_SimpleExportImport/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var friends_1 = require("./friends");
+var dev = new friends_1.Developer("Thomas");
+dev.knowsTypeScript = true;
+console.log(dev.firstName);
+console.log("knows TypeScript: " + dev.knowsTypeScript);
---
+++ 08_Module/02_modules_MultipleExportImport/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var friends_1 = require("./friends");
+var dev = new friends_1.Developer("Julia");
+dev.knowsTypeScript = true;
+var boarder = new friends_1.Skateboarder("Thomas");
+boarder.makeKickflip();
